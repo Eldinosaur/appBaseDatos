57ff5a74b781a817b28cfa57380ef30f83a5244b
Eldinosaur <82054019+Eldinosaur@users.noreply.github.com>
2022-07-07 17:09:13 -0500

    1

--- app/src/main/java/dev/eldinosaur/appbasedatos/PetsActivity.kt
+++ app/src/main/java/dev/eldinosaur/appbasedatos/PetsActivity.kt
@@ -3,9 +3,9 @@ package dev.eldinosaur.appbasedatos
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class MainActivity : AppCompatActivity() {
+class PetsActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_pets)
     }
 }
