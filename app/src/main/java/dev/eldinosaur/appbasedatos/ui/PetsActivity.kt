ab3b4d22ef939222146c04b1956c6d475b548570
Eldinosaur <82054019+Eldinosaur@users.noreply.github.com>
2022-07-07 18:33:22 -0500

    pets activity

--- app/src/main/java/dev/eldinosaur/appbasedatos/ui/PetsActivity.kt
+++ app/src/main/java/dev/eldinosaur/appbasedatos/ui/PetsActivity.kt
@@ -8,17 +8,33 @@ import dev.eldinosaur.appbasedatos.databinding.ActivityPetsBinding
 class PetsActivity : AppCompatActivity() {
 
     private lateinit var binding:ActivityPetsBinding
+    //private lateinit var adapter:PetAdapter
+    private val adapter:PetAdapter by lazy{
+        PetAdapter()
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityPetsBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        init()
+        setupAdapter()
         eventos()
+        loadData()
     }
     private fun eventos(){
         binding.fabRegister.setOnClickListener{
            startActivity(Intent(this, RegisterActivity::class.java))
         }
+    }
+    private fun init(){
+        binding.includePets.txtTitulo.text = "Mis Mascotas"
+    }
+    private fun setupAdapter(){
+        binding.rvPets.adapter = adapter
+    }
+    private fun loadData(){
+
     }
 }
--- app/src/main/java/dev/eldinosaur/appbasedatos/ui/RegisterActivity.kt
+++ app/src/main/java/dev/eldinosaur/appbasedatos/ui/RegisterActivity.kt
@@ -3,10 +3,30 @@ package dev.eldinosaur.appbasedatos.ui
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import dev.eldinosaur.appbasedatos.R
+import dev.eldinosaur.appbasedatos.databinding.ActivityRegisterBinding
+import dev.eldinosaur.appbasedatos.ui.database.AppDatabase
 
 class RegisterActivity : AppCompatActivity() {
+
+    private lateinit var binding:ActivityRegisterBinding
+    private val appDatabase:AppDatabase by lazy{
+        AppDatabase.getInstancia(this)
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_register)
+        binding = ActivityRegisterBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        init()
+        events()
+    }
+    private fun init(){
+        binding.includeRegister.txtTitulo.text = "Registrar Mascota"
+        binding.btnGrabar.text = "Registrar"
+        binding.edtNombre.requestFocus()
+    }
+    private fun events(){
+        
     }
 }
